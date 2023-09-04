e37e8dce74dc8753c5937dadf991b83e7a4ccc87
nomansum <nomanshiblisunny@gmail.com>
2023-09-04 09:25:28 +0600

    Props Drilling

--- src/index.js
+++ src/index.js
@@ -36,24 +36,61 @@ const inlineStyling = {
   marginTop: "0.5rem",
 };
 const Booklist = () => {
+
+  const someValue = "ShakeAndBake";
+  const displayValue = ()=>{
+    console.log(someValue);
+  }
+
   return (
     <section className="booklist">
+      {/* <EventExamples></EventExamples> */}
       {books.map((book, index) => {
         //const { img, title, author, id } = book;
-        return <Book {...book} key={book.id}></Book>;
+        return <Book {...book} key={book.id} displayValue = {displayValue}></Book>;
       })}
     </section>
   );
 };
 
+// const EventExamples = () => {
+//   const handleButtonClick = ()=>{
+//     alert("Handle Form INput");
+//   }
+//   const handleFormInput = (e)=>{
+//     //console.log(e);
+//      console.log("Something was typed");
+//   }
+//    const handleFormSubmission = (e)=>{
+//     e.preventDefault();
+//     //console.log(e);
+//      console.log("Form Submitted");
+//   }
+//   return <section>
+//     <form onSubmit={handleFormSubmission}>
+//       <h2>Typical Form</h2>
+//       <input type="text" onChange={handleFormInput} name="example" style={{margin:'1rem 0'}}></input>
+    
+//     <div>
+//       <button type="submit">Submit</button>
+//     </div>
+//     </form>
+//     <button onClick={handleButtonClick}>click me</button>
+//   </section>;
+// };
+
 const Book = (props) => {
   //const title = "This is a good book";
+  const displayTitle=()=>{
+    console.log(props.title);
+  }
   return (
     <article className="book">
       <img src={props.img} />
       <h2>{props.title}</h2>
       <h4 style={inlineStyling}>{props.author}</h4>
       {props.children}
+      <button onClick={props.displayValue}>Display Title</button>
     </article>
   );
 };
